b30799d1c8e9b947d85bd9002f5851dae3449409
Travis CI User <travis@example.org>
2019-08-17 17:56:08 +0000

    Documentation for killercup/cargo-edit@8f8028e

---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["cargo_metadata"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"cargo_metadata/enum.Error.html\" title=\"enum cargo_metadata::Error\">Error</a>",synthetic:false,types:["cargo_metadata::errors::Error"]},];
+implementors["cookie_store"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"cookie_store/struct.IdnaErrors.html\" title=\"struct cookie_store::IdnaErrors\">IdnaErrors</a>",synthetic:false,types:["cookie_store::IdnaErrors"]},];
+implementors["failure"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
